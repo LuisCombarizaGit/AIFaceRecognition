7280a7db79bcd7cc64e1d36129f090e59f8dbd06
Luis Combariza <luiscombariza@Luiss-MacBook-Air.local>
2020-11-27 19:51:10 -0700

    Changed project to be different than original, now making a face recognition program

--- main.py
+++ main.py
@@ -1,34 +1,20 @@
-######################  AI visual recognition #################
+######################  AI FACE recognition #################
 # Luis Combariza Nov 27,2020
 # luis_combariza@outlook.com
 
 import cv2
 import numpy as np
+import face_recognition
 
-# Setting frame
-frameWidth = 640
-frameHeight = 480
-cap = cv2.VideoCapture(0)
-cap.set(3, frameWidth)
-cap.set(4, frameHeight)
-cap.set(10,150)
-
-myColors = [[5,107,0,19,255,255],
-            [133,56,0,159,156,255],
-            [57,76,0,100,255,255]]
+imgElon = face_recognition.load_image_file("Elon_Musk_One.jpg")
+imgElon = cv2.cvtColor(imgElon,cv2.COLOR_BGR2RGB)
+imgTest = face_recognition.load_image_file("Elon_Musk_Two.jpeg")
+imgTest = cv2.cvtColor(imgTest,cv2.COLOR_BGR2RGB)
 
 
-def findColor(img,myColors):
-    imgHSV = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
-    for color in myColors:
-        lower = np.array(color[0:3])
-        upper = np.array(color[3:6])
-        mask = cv2.inRange(imgHSV,lower,upper)
-        cv2.imshow(str(color[0]),mask)
 
-while True:
-    success, img = cap.read()
-    findColor(img, myColors)
-    cv2.imshow("Result", img)
-    if cv2.waitKey(1) and 0xFF == ord('q'):
-        break
+
+
+cv2.imshow('Elon Musk',imgElon)
+cv2.imshow('Elon Test',imgTest)
+cv2.waitKey(0)
