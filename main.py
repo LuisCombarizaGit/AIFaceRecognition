7c48df044ab965f9115794ab6fe05be42d8a0c4b
Luis Combariza <luiscombariza@Luiss-MacBook-Air.local>
2020-11-27 18:15:28 -0700

    Bug with camara - does not want to open correctly-

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # luis_combariza@outlook.com
 
 import cv2
-import numpy
+import numpy as np
 
 # Setting frame
 frameWidth = 640
@@ -13,12 +13,22 @@ cap.set(3, frameWidth)
 cap.set(4, frameHeight)
 cap.set(10,150)
 
-def findColor(img):
-    imgHSV = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+myColors = [[5,107,0,19,255,255],
+            [133,56,0,159,156,255],
+            [57,76,0,100,255,255]]
 
+
+def findColor(img,myColors):
+    imgHSV = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+    for color in myColors:
+        lower = np.array(color[0:3])
+        upper = np.array(color[3:6])
+        mask = cv2.inRange(imgHSV,lower,upper)
+        cv2.imshow(str(color[0]),mask)
 
 while True:
     success, img = cap.read()
+    findColor(img, myColors)
     cv2.imshow("Result", img)
     if cv2.waitKey(1) and 0xFF == ord('q'):
         break
