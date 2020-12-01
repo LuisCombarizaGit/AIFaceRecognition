b0393fff00da20c80b23f46d4dd482a25a62afa5
Luis Combariza <luiscombariza@Luiss-MacBook-Air.local>
2020-11-30 20:43:45 -0700

    Encoding bug fixed -- Picture folder gets hidden files from git that try to be encoded as pictures in program and couse an error

--- RealTimeFaceRecognition.py
+++ RealTimeFaceRecognition.py
@@ -10,6 +10,7 @@ import os
 path = 'PictureFolder'
 images = []
 classNames = []
+newClassName = classNames[1:]
 myList = os.listdir(path)
 print(myList)
 
@@ -17,12 +18,14 @@ for cl in myList:
     curImg = cv2.imread(f'{path}/{cl}')
     images.append(curImg)
     classNames.append(os.path.splitext(cl)[0])
+print(classNames[1:])
+
 
 # Encode given images
 def findEncodings(images):
     encodeList = []
     for img in images:
-        img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         encode = face_recognition.face_encodings(img)[0]
         encodeList.append(encode)
 
@@ -31,12 +34,22 @@ def findEncodings(images):
 encodeListKnown = findEncodings(images)
 print('Encoding Complete')
 
-# Initialize web cam:
-cap = cv2.VideoCapture(0)
-
-while True:
-    success, img = cap.read()
-    imgS = cv2.resize(img,(0,0),None,)
+# # Initialize web cam:
+# cap = cv2.VideoCapture(0)
+#
+# while True:
+#     success, img = cap.read()
+#     imgS = cv2.resize(img,(0,0),None,0.25,0.25)
+#     imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+#
+#     facesCurFrame = face_recognition.face_locations(imgS)
+#     encodesCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
+#
+#     # Grav a single face location and its encode
+#     for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
+#         matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+#         faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+#         print(faceDis)
 
 # Check the given picture for face recognition
 # faceLoc = face_recognition.face_locations(imgElon)[0]
