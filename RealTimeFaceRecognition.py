a4f0bfdc5e93022a527594dcfc0bfa0f8d71ec64
Luis Combariza <luiscombariza@Luiss-MacBook-Air.local>
2020-11-30 19:53:06 -0700

    Finished picture recognition file , now coding real time face recognition based from the picture recognition

---
+++ RealTimeFaceRecognition.py
@@ -0,0 +1,12 @@
+######################  AI FACE recognition #################
+# Luis Combariza Nov 27,2020
+# luis_combariza@outlook.com
+
+import cv2
+import numpy as np
+import face_recognition
+
+imgElon = face_recognition.load_image_file("Elon_Musk_One.jpg")
+imgElon = cv2.cvtColor(imgElon,cv2.COLOR_BGR2RGB)
+imgTest = face_recognition.load_image_file("Elon_Musk_Two.jpeg")
+imgTest = cv2.cvtColor(imgTest,cv2.COLOR_BGR2RGB)
--- main.py
+++ main.py
@@ -11,14 +11,20 @@ imgElon = cv2.cvtColor(imgElon,cv2.COLOR_BGR2RGB)
 imgTest = face_recognition.load_image_file("Elon_Musk_Two.jpeg")
 imgTest = cv2.cvtColor(imgTest,cv2.COLOR_BGR2RGB)
 
+# Check the given picture for face recognition
 faceLoc = face_recognition.face_locations(imgElon)[0]
 encodeElon = face_recognition.face_encodings(imgElon)[0]
 cv2.rectangle(imgElon,(faceLoc[3],faceLoc[0]),(faceLoc[1],faceLoc[2]),(255,0,255),2)
 
+# Check the given picture for face recognition
 faceLocTest = face_recognition.face_locations(imgTest)[0]
 encodeTest = face_recognition.face_encodings(imgTest)[0]
 cv2.rectangle(imgTest,(faceLocTest[3],faceLocTest[0]),(faceLocTest[1],faceLocTest[2]),(255,0,255),2)
 
+results = face_recognition.compare_faces([encodeElon],encodeTest)
+faceDis = face_recognition.face_distance([encodeElon],encodeTest)
+cv2.putText(imgTest,f'{results}{round(faceDis[0],2)}',(50,50),cv2.FONT_HERSHEY_PLAIN,1,(0,0,255),2)
+#print(results)
 
 
 cv2.imshow('Elon Musk',imgElon)
